ffb08837148ad9d031e026b2a9b6009f50d58018
s6nchez <eoaeoe@gmail.com>
2026-05-20 16:51:22 +0200

    Dashboard más completo

--- apps/backend/src/modules/dashboard/dashboard.controller.js
+++ apps/backend/src/modules/dashboard/dashboard.controller.js
@@ -1,9 +1,21 @@
-import { getLatestPoolLevels } from "./dashboard.service.js";
+import {
+  getLatestPoolLevels,
+  getDashboardSummary,
+} from "./dashboard.service.js";
 
 export async function getLatestPoolLevelsController(req, res, next) {
   try {
     const data = await getLatestPoolLevels();
     return res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function getDashboardSummaryController(req, res, next) {
+  try {
+    const dashboard = await getDashboardSummary();
+    return res.status(200).json(dashboard);
   } catch (error) {
     next(error);
   }
