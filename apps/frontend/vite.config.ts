3522fad2b14591a6e8b15bfdeb900484b80bedfd
s6nchez <eoaeoe@gmail.com>
2026-07-08 10:57:23 +0200

    Corrección imágenes

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,6 +3,18 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  server: {
+    proxy: {
+      "/uploads": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [
     react(),
     VitePWA({
